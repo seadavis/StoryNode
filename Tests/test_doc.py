bfd02384f02871cfc81199cfbd8c28c236d3b626
seadavis <seadav.17@gmail.com>
2021-05-02 16:55:20 -0700

    Implemented swapping, except multiple swaps

--- Tests/test_doc.py
+++ Tests/test_doc.py
@@ -9,7 +9,7 @@ def test_one_to_one_replacement():
     doc = Document(sample_text)
     doc2 = Document("word word word word word word hello")
     span = doc.span(2, 3)
-    span_swap = doc2.span(7, 8)
+    span_swap = doc2.span(6, 7)
     doc.swap(span, span_swap)
     new_text = doc.print()
     assert new_text == "Python code hello the cleanest code around unlike C++ which is garbage nonsense"
@@ -17,7 +17,7 @@ def test_one_to_one_replacement():
 
 def test_multiple_replacements():
 
-    ample_text = "Python code is the cleanest code around unlike C++ which is garbage nonsense"
+    sample_text = "Python code is the cleanest code around unlike C++ which is garbage nonsense"
     doc = Document(sample_text)
     span = doc.span(5, 6)
     span2 = doc.span(12, 13)
@@ -25,7 +25,7 @@ def test_multiple_replacements():
 
     doc2 = Document("the joker fights guys batman is evil")
     doc3 = Document("highlighting textbooks for fun")
-    span_swap = doc2.span(5, 6)
+    span_swap = doc2.span(4, 5)
     span_swap2 = doc3.span(0, 1)
     span_swap3 = doc3.span(1, 3)
 
@@ -34,14 +34,14 @@ def test_multiple_replacements():
     doc.swap(span3, span_swap3)
 
     new_text = doc.print()
-    assert new_text == "Python code is the cleanest batman textbooks for C++ which is garbage highlighting"
+    assert new_text == "Python code is the cleanest batman around unlike textbooks for is garbage highlighting"
 
 def test_one_to_one_replacement_same_start_end():
     sample_text = "Python code is the cleanest code around unlike C++ which is garbage nonsense"
     doc = Document(sample_text)
     span = doc.span(5, 6)
     doc2 = Document("the joker fights guys batman is evil")
-    span_swap = doc2.span(5, 6)
+    span_swap = doc2.span(4, 5)
     doc.swap(span, span_swap)
     new_text = doc.print()
     assert new_text == "Python code is the cleanest batman around unlike C++ which is garbage nonsense"
@@ -51,7 +51,7 @@ def test_n_to_n_replacement():
     doc = Document(sample_text)
     span = doc.span(0, 7)
     doc2 = Document("my adult nephew enjoys crafts and cutting paper like a two year old")
-    span_swap = doc2.span(7, 14)
+    span_swap = doc2.span(6, 14)
     doc.swap(span, span_swap)
     new_text = doc.print()
 
@@ -62,17 +62,17 @@ def test_n_to_n_replacement_same_start_end():
     doc = Document(sample_text)
     span = doc.span(6, 9)
     doc2 = Document("my mom told me to button up jacket young man its cold outside")
-    span_swap = doc2.span(6, 9)
+    span_swap = doc2.span(5, 9)
     doc.swap(span, span_swap)
     new_text = doc.print()
-    assert new_text == "Python code is the cleanest code button up jacket which is garbage nonsense"
+    assert new_text == "Python code is the cleanest code button up jacket young which is garbage nonsense"
 
 def test_shorter_replacement():
     sample_text = "Python code is the cleanest code around unlike C++ which is garbage nonsense"
     doc = Document(sample_text)
     span = doc.span(4, 9)
     doc2 = Document("down dog, get off of the couch")
-    span_swap = doc2.span(0, 1)
+    span_swap = doc2.span(0, 2)
     doc.swap(span, span_swap)
     new_text = doc.print()
     assert new_text == "Python code is the down dog which is garbage nonsense"
@@ -86,7 +86,7 @@ def test_longer_replacement():
     span_swap = doc2.span(5,12)
     doc.swap(span, span_swap)
     new_text = doc.print()
-    assert new_text == "Python code is the cleanest code around unlike button up jacket young man its cold outside is garbage nonsense"
+    assert new_text == "Python code is the cleanest code around unlike button up jacket young man its cold is garbage nonsense"
 
 
 def test_original_still_prints_after_replacement():
--- src/core/document.py
+++ src/core/document.py
@@ -13,20 +13,30 @@ class SwapSpan:
     new_span - the span being "swapped in" or the new span
     """
     def __init__(self, new_span, swapped_span):
-        self.span = span
         self.new_span = new_span
         self.swapped_span = swapped_span
 
     @property
     def swap_start(self):
-        return self.swapped_span.start_index
+        return self.swapped_span.swap_start
 
     @property
     def swap_end(self):
-        return self.swapped_span.end_index
+        return self.swapped_span.swap_end
 
     def swapped_text(self):
         return self.new_span.sentence
+
+    def intersects(self, other):
+        if self.swap_start >= other.swap_start and self.swap_start <= other.swap_end:
+            return True
+        if self.swap_end >= other.swap_start and self.swap_end <= other.swap_end:
+            return True
+        if other.swap_start >= self.swap_start and other.swap_start <= self.swap_end:
+            return True
+        if other.swap_end >= self.swap_start and other.swap_end <= self.swap_end:
+            return True
+        return False
 
 class Document:
 
@@ -47,39 +57,51 @@ class Document:
         span_2 the span being swapped into span_1
     """
     def swap(self, span_1, span_2):
-        self.swap_list = self.swap_list.append(SwappedSpans(span_2, span_1)
+        new_swap = SwapSpan(span_2, span_1)
+        self.swap_list.append(new_swap)
+        self.swap_list = sorted(self.swap_list, key=lambda s: s.swap_start)
 
     """
     Prints out the current document
-    original_document - is set to true prints out the original document
+    original_document - if set to true prints out the original document
     before swaps
     """
     def print(self, original_document = False):
+        
         if original_document or len(self.swap_list) == 0:
             return self.doc[0:len(self.doc)].text
+        
         else:
 
             printed_doc = ""
+            last_swap_end = 0
+            last_index = len(self.swap_list) - 1
 
-            for (index, span) in self.swap_list:
+            for (index, swapped_span) in enumerate(self.swap_list):
 
-                swapped_start = span.swap_start
-                swapped_end = span.swapped_end
-                printed_doc = self.doc[last_swapped_end:swapped_start].text + span.swapped_text + self.doc[swapped_end]
+                previous_text = ""
+                next_text = ""
+
+                if index > 0 and self.swap_list[index - 1].swap_end != swapped_span.swap_start:
+                    previous_end = self.swap_list[index - 1].swap_end
+                    previous_span = TextSpan(self.doc[previous_end:swapped_span.swap_start])
+                    previous_text = previous_span.sentence
+
+                elif index == 0 and swapped_span.swap_start > 0:
+                    previous_text = self.doc[0:swapped_span.swap_start].text
+            
+                swap_text = swapped_span.new_span.sentence
+                components = [printed_doc, previous_text, swap_text]
+                non_empty_components = (t for t in components if len(t) > 0 and (not t is None))
+                printed_doc =  " ".join(non_empty_components)
+
+            last_span = self.swap_list[last_index]
+            if last_span.swap_end < len(self.doc):
+                next_text = self.doc[swapped_span.swap_end:(len(self.doc))].text
+                printed_doc = printed_doc + " " + next_text
 
             return printed_doc
 
 
-
     def span(self, start, end):
         return TextSpan(self.doc[start:end])
-
-    """
-    gets the spans from
-    the swap list that intersect the given
-    span
-
-    """
-    def get_swap_intersections(self, span):
-        return (s for s in self.swap_list if s.intersects(span))
-
